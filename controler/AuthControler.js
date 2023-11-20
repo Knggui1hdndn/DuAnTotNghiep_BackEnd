b23fcc8f84d3e92bb2d95f03094f4390e5374764
khang <khangndph20612@fpt.edu.vn>
2023-11-21 01:12:23 +0700

    f

--- controler/AuthControler.js
+++ controler/AuthControler.js
@@ -128,9 +128,10 @@ const signUpLocal = async (req, res, next) => {
       address: address,
       phoneNumber: phoneNumber,
       password: password,
-       roleType:roleType,
-        avatar: "https://gocsuckhoe.com/wp-content/uploads/2020/09/avatar-facebook.jpg",
-    });
+ 
+      roleType:roleType,
+      avatar: "https://gocsuckhoe.com/wp-content/uploads/2020/09/avatar-facebook.jpg",
+     });
 
     const userSave = await User.create(newUser);
 
