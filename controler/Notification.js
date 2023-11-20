8adb76dfb79bbc983471f3e4b54c501c6ba81e24
Khang-PH20612 <103960248+Knggui1hdndn@users.noreply.github.com>
2023-11-21 00:46:32 +0700

    Update Notification.js

--- controler/Notification.js
+++ controler/Notification.js
@@ -1,7 +1,36 @@
 const admin = require("../app"); // Đường dẫn tới app.js
 const TokenFcm = require("../model/tokenFcm");
+const Notification = require("../model/notification");
 
- 
+const getNotification = async (req, res) => {
+  try {
+    const notifications = await Notification.find({ idReceve: req.user._id });
+    res.status(200).send(notifications);
+  } catch (error) {
+    console.error('Error fetching notifications:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+
+const updateNotification = async (req, res) => {
+  try {
+    const notifications = await Notification.findOneAndUpdate(
+      { _id: req.query._id },
+      { isSeen: true },
+      { new: true }
+    );
+
+    if (notifications) {
+      res.status(200).send(notifications);
+    } else {
+      res.status(404).json({ error: 'Notification not found' });
+    }
+  } catch (error) {
+    console.error('Error updating notification:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
 
 function sendNotification(registrationTokens, notification) {
   const message = {
@@ -21,4 +50,4 @@ function sendNotification(registrationTokens, notification) {
     });
 }
 
-module.exports = { sendNotification };
+module.exports = { sendNotification, getNotification, updateNotification };
